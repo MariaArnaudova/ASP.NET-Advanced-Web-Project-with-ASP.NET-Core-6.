6242ae7e16826b8c8cc3aa6c481339db856614a0
MariaCode <m.p.arnaudova@gmail.com>
2023-07-13 20:11:58 +0300

    ClaimsPrincipalExtensions

--- ArtStroke.Web.Infrastructure/Extentions/ClaimsPrincipalExtensions.cs
+++ ArtStroke.Web.Infrastructure/Extentions/ClaimsPrincipalExtensions.cs
@@ -1,16 +1,12 @@
 ﻿
-
-using System.Security.Claims;
-
 namespace ArtStroke.Web.Infrastructure.Extentions
 {
     using System.Security.Claims;
     public static class ClaimsPrincipalExtensions
     {
-        public static string? GetId(this ClaimsPrincipal user)
+        public static string GetId(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
         }
-
     }
 }
