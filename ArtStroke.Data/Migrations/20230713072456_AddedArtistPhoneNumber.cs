3bc1db63ecbfc46ed9cd20db796bab6b4c56d98c
MariaCode <m.p.arnaudova@gmail.com>
2023-07-13 12:39:13 +0300

    Seeding data in database

---
+++ ArtStroke.Data/Migrations/20230713072456_AddedArtistPhoneNumber.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtStroke.Data.Migrations
+{
+    public partial class AddedArtistPhoneNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Artists",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Artists");
+        }
+    }
+}
