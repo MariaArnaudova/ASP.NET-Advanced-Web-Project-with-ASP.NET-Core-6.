6254b83f28c61db443eb29473b1d2d444a5395a9
MariaCode <m.p.arnaudova@gmail.com>
2023-07-14 16:38:41 +0300

    Created ArtWorkStyleViewModel and IStyleService

---
+++ ArtStroke.Web.ViewModels/Styles/ArtWorkStyleViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Security.Principal;
+
+namespace ArtStroke.Web.ViewModels.Styles
+{
+    public class ArtWorkStyleViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;    
+    }
+}
---
+++ ArtWork.Services.Data/Interfaces/IStyleService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStroke.Services.Data.Interfaces
+{
+    internal interface IStyleService
+    {
+    }
+}
