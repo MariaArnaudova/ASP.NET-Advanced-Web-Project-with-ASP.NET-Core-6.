21906e2acabd6293dfcbc9a6c9f008b01f40cfe8
MariaCode <m.p.arnaudova@gmail.com>
2023-07-15 23:49:37 +0300

    Enums added

---
+++ ArtStroke.Web.ViewModels/ArtWork/Enums/ArtWorkSorting.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ArtStroke.Web.ViewModels.ArtWork.Enums
+{
+    public enum ArtWorkSorting
+    {
+        Newest = 0,
+        Oldest = 1,
+        Techique = 2,
+        CreatingYearAscending = 3,
+        CreatingYearDescending = 4,
+
+    }
+}
