401de66c021a9e71adb5538188b87a68c318ede7
MariaCode <m.p.arnaudova@gmail.com>
2023-07-17 21:35:22 +0300

    Implement Details functionality

---
+++ ArtStroke.Web.ViewModels/ArtWork/ArtworkDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtStroke.Web.ViewModels.Artist;
+
+namespace ArtStroke.Web.ViewModels.ArtWork
+{
+    public class ArtworkDetailsViewModel : ArtworkAllViewModel
+    {
+
+        public string Technique { get; set; } = null!;
+
+        [Display(Name = "Width on desighed artwork")]
+        public int Width { get; set; }
+
+
+        [Display(Name = "Height on desighed artwork")]
+        public int Height { get; set; }
+
+        public ArtistInfoOnArtworkViewModel Artist { get; set; } = null!;
+    }
+}
---
+++ ArtStroke.Web.ViewModels/Artist/ArtistInfoOnArtworkViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStroke.Web.ViewModels.Artist
+{
+    public class ArtistInfoOnArtworkViewModel
+    {
+        [Display(Name="Author name of original")]
+        public string Name { get; set; } = null!;
+        public string Biography { get; set; } = null!;
+    }
+}
