24dce67142e4852e81c3299b6dd123f3946860d7
MariaCode <m.p.arnaudova@gmail.com>
2023-07-17 18:36:17 +0300

    Implement  PrintDesign functionality

---
+++ ArtStroke.Web.ViewModels/PrintDesigns/AllPrintDesignsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStroke.Web.ViewModels.PrintDesigns
+{
+    public class AllPrintDesignsViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string CreatorName { get; set; } = null!;
+
+        public Guid? UserId { get; set; }
+        public Guid ArtWorkId { get; set; }
+    }
+}
