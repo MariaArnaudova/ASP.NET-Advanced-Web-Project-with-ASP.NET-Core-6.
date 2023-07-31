a75127f4d122cfed26bedbfa9be433ca94c4ee90
MariaCode <m.p.arnaudova@gmail.com>
2023-07-31 22:33:52 +0300

    Set migrations

---
+++ ArtStroke.Data/Migrations/20230730163651_SetPrintTechniquesEventsIdToGuid.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtStroke.Data.Migrations
+{
+    public partial class SetPrintTechniquesEventsIdToGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("30391142-d731-49ba-b44c-796b810ec033"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("5de207fe-b605-4b1c-a631-41056a597f36"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("695c448b-bb28-4779-97f0-80132b622f2c"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("e6d9bb77-ec50-43de-87a3-19655a09db17"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "PrintDesigns",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "NewTechiqueArts",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "ArtEvents",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.InsertData(
+                table: "ArtWorks",
+                columns: new[] { "Id", "ArtistId", "CreatingYear", "Height", "ImageUrl", "IsDesignedInPrint", "StyleId", "Technique", "Title", "Width" },
+                values: new object[,]
+                {
+                    { new Guid("7d9bfaf4-a5ea-4d63-8022-0aeb8b542a22"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 35, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sea-sunset-maria-arnaudova.jpg", false, 3, "Oil on canvas", "Sunset over the sea", 50 },
+                    { new Guid("9e681d34-d085-4414-811f-5f571598131f"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 25, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/blue-boat-maria-arnaudova.jpg", false, 1, "Oil on canvas", "Single Boat", 35 },
+                    { new Guid("da8f0a6b-144e-46a2-b694-1d6162b345a8"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/split-pomegranate-maria-arnaudova.jpg", false, 2, "Acril on canvas", "Freshnest", 50 },
+                    { new Guid("f0e803dc-0f6f-442e-a2bb-0a66345be6fe"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/swirling-of-life-maria-arnaudova.jpg", false, 4, "Aquarel", "Swirling", 35 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("7d9bfaf4-a5ea-4d63-8022-0aeb8b542a22"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("9e681d34-d085-4414-811f-5f571598131f"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("da8f0a6b-144e-46a2-b694-1d6162b345a8"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("f0e803dc-0f6f-442e-a2bb-0a66345be6fe"));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "PrintDesigns",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "NewTechiqueArts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ArtEvents",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.InsertData(
+                table: "ArtWorks",
+                columns: new[] { "Id", "ArtistId", "CreatedOn", "CreatingYear", "Height", "ImageUrl", "IsActive", "IsDesignedInPrint", "StyleId", "Technique", "Title", "Width" },
+                values: new object[,]
+                {
+                    { new Guid("30391142-d731-49ba-b44c-796b810ec033"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/split-pomegranate-maria-arnaudova.jpg", false, false, 2, "Acril on canvas", "Freshnest", 50 },
+                    { new Guid("5de207fe-b605-4b1c-a631-41056a597f36"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 35, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sea-sunset-maria-arnaudova.jpg", false, false, 3, "Oil on canvas", "Sunset over the sea", 50 },
+                    { new Guid("695c448b-bb28-4779-97f0-80132b622f2c"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 25, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/blue-boat-maria-arnaudova.jpg", false, false, 1, "Oil on canvas", "Single Boat", 35 },
+                    { new Guid("e6d9bb77-ec50-43de-87a3-19655a09db17"), new Guid("9acb423b-f83d-4a6f-a4e3-d28271e0e828"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/swirling-of-life-maria-arnaudova.jpg", false, false, 4, "Aquarel", "Swirling", 35 }
+                });
+        }
+    }
+}
