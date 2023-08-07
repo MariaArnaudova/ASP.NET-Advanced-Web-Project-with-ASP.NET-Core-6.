b4105140920638ce6dc44659601bacd95ff53b79
MariaCode <m.p.arnaudova@gmail.com>
2023-08-07 17:26:28 +0300

    Added Migrations/20230807141820_AddUserFirtsNameAndLastName.cs

---
+++ ArtStroke.Data/Migrations/20230807141820_AddUserFirtsNameAndLastName.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtStroke.Data.Migrations
+{
+    public partial class AddUserFirtsNameAndLastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("605ec1df-b6a8-4b8d-97bf-199b904894f4"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("82d6c357-5443-4041-8687-a08e1641fe1a"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("a70fe6a7-e9ed-4422-839a-38555cc32596"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("df50606e-acea-4e2e-a2d1-9af8e5cc5f1e"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "Test");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "TestovUser");
+
+            migrationBuilder.InsertData(
+                table: "ArtWorks",
+                columns: new[] { "Id", "ArtistId", "CreatingYear", "Height", "ImageUrl", "IsDesignedInPrint", "StyleId", "Technique", "Title", "Width" },
+                values: new object[,]
+                {
+                    { new Guid("89f05774-b5cc-4281-bff3-62baf4a9449f"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/swirling-of-life-maria-arnaudova.jpg", false, 4, "Aquarel", "Swirling", 35 },
+                    { new Guid("97357f6b-f536-40a1-a3d8-d42ed0d92392"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 25, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/blue-boat-maria-arnaudova.jpg", false, 1, "Oil on canvas", "Single Boat", 35 },
+                    { new Guid("d20d810d-faed-495d-9ef6-1da9069e37fa"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 35, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sea-sunset-maria-arnaudova.jpg", false, 3, "Oil on canvas", "Sunset over the sea", 50 },
+                    { new Guid("ed314586-e35c-42d3-a651-1aa71da6c37e"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/split-pomegranate-maria-arnaudova.jpg", false, 2, "Acril on canvas", "Freshnest", 50 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("89f05774-b5cc-4281-bff3-62baf4a9449f"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("97357f6b-f536-40a1-a3d8-d42ed0d92392"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("d20d810d-faed-495d-9ef6-1da9069e37fa"));
+
+            migrationBuilder.DeleteData(
+                table: "ArtWorks",
+                keyColumn: "Id",
+                keyValue: new Guid("ed314586-e35c-42d3-a651-1aa71da6c37e"));
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "ArtWorks",
+                columns: new[] { "Id", "ArtistId", "CreatedOn", "CreatingYear", "Height", "ImageUrl", "IsActive", "IsDesignedInPrint", "StyleId", "Technique", "Title", "Width" },
+                values: new object[,]
+                {
+                    { new Guid("605ec1df-b6a8-4b8d-97bf-199b904894f4"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 25, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/blue-boat-maria-arnaudova.jpg", false, false, 1, "Oil on canvas", "Single Boat", 35 },
+                    { new Guid("82d6c357-5443-4041-8687-a08e1641fe1a"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 35, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/sea-sunset-maria-arnaudova.jpg", false, false, 3, "Oil on canvas", "Sunset over the sea", 50 },
+                    { new Guid("a70fe6a7-e9ed-4422-839a-38555cc32596"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/split-pomegranate-maria-arnaudova.jpg", false, false, 2, "Acril on canvas", "Freshnest", 50 },
+                    { new Guid("df50606e-acea-4e2e-a2d1-9af8e5cc5f1e"), new Guid("4db15f1b-f1c7-4cda-a6fc-a8f1dc4c4d2b"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 50, "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/swirling-of-life-maria-arnaudova.jpg", false, false, 4, "Aquarel", "Swirling", 35 }
+                });
+        }
+    }
+}
