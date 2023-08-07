5c74d6b202b63d7427cd2a113497fd388ab2430c
MariaCode <m.p.arnaudova@gmail.com>
2023-08-07 17:01:19 +0300

    Configure Login and Register pages

---
+++ ArtStroke.Web/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ArtStroke.Web.Controllers
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using ArtStroke.Data.Models;
+    public class UserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserStore<ApplicationUser> userStore;
+
+        public UserController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
