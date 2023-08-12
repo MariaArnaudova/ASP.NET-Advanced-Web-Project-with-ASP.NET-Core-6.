2a3f2ad4e6e93d05b7c669e66a49f3c0dc8d6d97
MariaCode <m.p.arnaudova@gmail.com>
2023-08-12 19:48:46 +0300

    Created ArtistServiceTest.cs

---
+++ ArtStroke.Services.Tests/ArtistServiceTest.cs
@@ -0,0 +1,102 @@
+
+namespace ArtStroke.Services.Tests
+{
+    using Microsoft.EntityFrameworkCore;
+    using ArtStroke.Data;
+    //using ArtStroke.Data.Models;
+    using ArtStroke.Services.Data;
+    using ArtStroke.Services.Data.Interfaces;
+    using static DatabaseSeeder;
+
+    public class ArtistServiceTests
+    {
+        private DbContextOptions<ArtStrokeDbContext> dbOptions;
+        private ArtStrokeDbContext dbContext;
+        private IArtistService artistService;
+        private IArtWorkService artWorkService;
+
+
+        [OneTimeSetUp]
+        public void OnTimeSetUp()
+        {
+
+            this.dbOptions = new DbContextOptionsBuilder<ArtStrokeDbContext>()
+                .UseInMemoryDatabase("ArtStrokeInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new ArtStrokeDbContext(this.dbOptions);
+            this.dbContext.Database.EnsureCreated();
+            SeedDatabase(this.dbContext);
+
+            this.artistService = new ArtistService(this.dbContext);
+            this.artWorkService =  new ArtWorkService(this.dbContext);
+        }
+
+        [Test]
+        public async Task HasArtistByUserIdAsyncShouldReturnTrueWhenExists()
+        {
+            string existingArtistId = ArtistUser.Id.ToString();
+            bool result = await this.artistService.HasArtistByUserIdAsync(existingArtistId);
+            Assert.True(result);
+        }
+
+        [Test]
+        public async Task HasArtistByUserIdAsyncShouldReturnFalseWhenDoNotExists()
+        {
+            string existingArtistId = "BB573B0F-06A3-484B-878D-792514CAE7DD";
+            bool result = await this.artistService.HasArtistByUserIdAsync(existingArtistId);
+            Assert.False(result);
+        }
+
+        [Test]
+        public async Task HasArtworkByIdAsyncShouldReturnTrueIfExists()
+        {
+            string artworkId = Artwork.Id.ToString();
+            string existingArtistId = ArtistUser.Id.ToString();
+            bool result = await this.artistService.HasArtworkByIdAsync(artworkId, existingArtistId);
+            Assert.True(result);
+        }
+
+        [Test]
+        public async Task HasArtworkByIdAsyncShouldReturnFalseIfDoNotExists()
+        {
+            string artworkId = Artwork.Id.ToString();
+            string existingArtistId = "BB573B0F-06A3-484B-878D-792514CAE7DD";
+            bool result = await this.artistService.HasArtworkByIdAsync(artworkId, existingArtistId);
+            Assert.False(result);
+        }
+
+        [Test]
+        public async Task GetArtistIdByUserIdAsyncShouldReturnTrueIfExists()
+        {
+            string artistId = Artist.Id.ToString();
+            string existingArtistByUserId = ArtistUser.Id.ToString();
+            string result = await this.artistService.GetArtistIdByUserIdAsync(existingArtistByUserId);
+            Assert.That(artistId, Is.EqualTo(result));
+        }
+
+        [Test]
+        public async Task GetArtistIdByUserIdAsyncShouldReturnFalseIfNotExists()
+        {
+            string artistId = ArtistUser.Id.ToString();
+            string existingArtistByUserId = ArtistUser.Id.ToString();
+            string result = await this.artistService.GetArtistIdByUserIdAsync(existingArtistByUserId);
+            Assert.That(artistId, Is.Not.EqualTo(result));
+        }
+
+        [Test]
+
+        public async Task ArtistExistByPhonenumberAsyncReturnTrueIfExists()
+        {
+            bool existArtistByPhoneNumber = await this.artistService.ArtistExistByPhonenumberAsync("+3593333333333");
+            Assert.True(existArtistByPhoneNumber);
+        }
+
+        [Test]
+        public async Task ArtistExistByPhonenumberAsyncReturnFalseIfNotExists()
+        {
+            bool existArtistByPhoneNumber = await this.artistService.ArtistExistByPhonenumberAsync("+3593333333344");
+            Assert.True(existArtistByPhoneNumber);
+        }
+    }
+}
---
+++ ArtStroke.Services.Tests/DatabaseSeeder.cs
@@ -0,0 +1,81 @@
+﻿
+
+namespace ArtStroke.Services.Tests
+{
+    using ArtStroke.Data;
+    using ArtStroke.Data.Models;
+    using System.Globalization;
+
+    public static class DatabaseSeeder
+    {
+
+        public static ApplicationUser ArtistUser;
+        public static Artist Artist;
+        public static ArtWork Artwork;
+        public static Style Style;
+
+
+        public static void SeedDatabase(ArtStrokeDbContext dbContext)
+        {
+            dbContext.Database.EnsureCreated(); 
+
+            ArtistUser = new ApplicationUser()
+            {
+                UserName = "Test",
+                NormalizedUserName = "TEST",
+                Email = "test@test.com",
+                NormalizedEmail = "TEST@TESTS.COM",
+                EmailConfirmed = true,
+                PasswordHash = "8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92",
+                SecurityStamp = "82a5dccd-a7a0-4656-a222-63d54c4fe3cb",
+                TwoFactorEnabled = false,
+                ConcurrencyStamp = "39eaa544-1469-4818-8e95-a29590c32a55",
+                FirstName = "Test",
+                LastName = "UserTest"
+            };
+
+            Artist = new Artist()
+            {
+                PhoneNumber = "+3593333333333",
+                User = ArtistUser,
+                Biography= "Test biography",
+                Name= "Test Name",
+                
+            };
+
+
+            Style = new Style()
+            {
+                Name = "Classic"
+            };
+
+            string yearString = "2022";
+            DateTime creatingYear;
+            DateTime.TryParseExact(yearString, "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out creatingYear);
+
+            Artwork = new ArtWork()
+            {
+                Title = "Single Boat",
+                Technique = "Oil on canvas",
+                StyleId = 1,
+                Width = 35,
+                Height = 25,
+                ImageUrl = "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/blue-boat-maria-arnaudova.jpg",
+                CreatingYear = creatingYear,
+                ArtistId = Artist.Id,
+                CreatedOn = creatingYear,
+                IsActive = true,
+                IsDesignedInPrint = true,
+                Style = Style,
+                PrintDesigns = new HashSet<PrintDesign>(),        
+            };
+
+
+            dbContext.Users.Add(ArtistUser);
+            dbContext.Artists.Add(Artist);
+            dbContext.ArtWorks.Add(Artwork);
+            dbContext.SaveChanges();
+        }
+
+    };
+}
